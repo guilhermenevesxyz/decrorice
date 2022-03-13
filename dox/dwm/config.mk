eb0c3af940998bdd8478d68097360ef5c9592c6f
decrovert <decrovertp@gmail.com>
2022-03-13 21:21:22 +0000

    improving the syntax in dwn/config.mk

--- dox/dwm/config.mk
+++ dox/dwm/config.mk
@@ -8,19 +8,16 @@ VERSION = 6.3-decrovert
 # Compiler and linker
 CC = gcc
 
-define SOLARIS
-endef
-
 # General paths
-PREFIX    = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
 
 # X11
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # Xinerama (uncomment if needed)
-#XINERAMALIBS  = -lXinerama
+#XINERAMALIBS = -lXinerama
 #XINERAMAFLAGS = -DXINERAMA
 
 # Freetype
@@ -31,14 +28,16 @@ FREETYPEINC = /usr/include/freetype2
 #FREETYPEINC = ${X11INC}/freetype2
 
 # Includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+INCS = -I$(X11INC) -I$(FREETYPEINC)
+LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
 
 # Flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS = -std=c17 -w -O2 ${INCS} ${CPPFLAGS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L \
+	-DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+
+CFLAGS = -std=c17 -w -O2 $(INCS) $(CPPFLAGS)
 
 # Debug mode (uncomment)
 #CFLAGS = -std=c17 -pedantic -Wall -Wextra -O0 ${INCS} ${CPPFLAGS}
 
-LDFLAGS = ${LIBS}
+LDFLAGS = $(LIBS)
